dd8c5f77f2cc49da571fe1df4604de5d2552bd8f
wlghC <jhscndact@gmail.com>
2025-03-12 00:50:36 -0400

    fix some error on [slug].js file

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,10 @@ import React from 'react'
 import { client, urlFor } from '../../lib/client'
 
 const ProductDetails = ({product, products}) => {
-  const {image, name, details, price} = products;
+  const {image, name, details, price} = product;
   return (
     <div>
-        <div className='product-detail-continer'>
+        <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
                     <img src={urlFor(image && image[0])}/>
