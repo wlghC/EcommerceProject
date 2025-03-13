e406865dd35a6d11e7459cd0cc1f4e00231352f5
wlghC <jhscndact@gmail.com>
2025-03-13 13:26:23 -0400

    Correct typo

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({products, bannerData}) => {
       <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
-        <p>Speakers of mnay variations passages</p>      
+        <p>Speakers of many variations passages</p>      
       </div>
       <div className='products-container'>
         {products?.map((product) => <Product key={product._id} product={product} />)}
